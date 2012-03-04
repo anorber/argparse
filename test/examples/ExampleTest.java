e5e65df7d56c08dfac4e9038c6540151eebe0073
anorber <anorber@kth.se>
2012-03-04 13:56:10 +0100

    updated example

--- test/examples/ExampleTest.java
+++ test/examples/ExampleTest.java
@@ -27,10 +27,10 @@ public class ExampleTest {
 		};
 		String expected = "" +
 				"opts:\n" +
-				"   -b \n" +
+				"   -b\n" +
 				"   -a con\n" +
 				"   --alpha one\n" +
-				"   --beta \n" +
+				"   --beta\n" +
 				"args:\n" +
 				"   -a\n" +
 				"   foo\n";
